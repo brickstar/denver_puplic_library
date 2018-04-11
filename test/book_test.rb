a33b656e0698c8966fce15f014d95b6141c2d69b
brickstar <brickstar@gmail.com>
2018-04-11 09:07:48 -0600

    adds setup

--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,16 @@
 require './test/test_helper'
+require './lib/book'
 
 class BookTest < Minitest::Test
 
+  def setup
+    @b = book = Book.new({author_first_name: "Harper",
+                          author_last_name: "Lee",
+                          title: "To Kill a Mockingbird",
+                          publication_date: "July 11, 1960"})
+  end
 
+  def test_it_exists
+    assert_instance_of Book, @b
+  end
 end
